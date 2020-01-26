921db6a57dcb82ea083a4d2cb8f7c17bf1dd4c3c
Erik Pellizzon <erikpellizzon1@gmail.com>
2020-01-26 20:20:18 +0100

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,31 @@
+# Copyright 2020 Erik Pellizzon
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import json
+import csv
+
+with open("locations.json") as json_file:
+    All = json.load(json_file)
+  
+with open('mccafe.csv', newline='') as f:
+    reader = csv.reader(f)
+    McCafe = list(reader)
+
+out = []
+
+for mc in McCafe:
+    name = mc[0]
+    for id in All:
+        if All[str(id)]['location'] == name:
+            out.append({"store_id": id, "location": name})
+            break
+            
+json_string = json.dumps(out, sort_keys=True, indent=4)
+
+with open("out.json", "w") as json_file:
+    json_file.write(json_string)
